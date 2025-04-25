fd3524289c36aaf017966e4c5da7dc81f4253494
Dobby114 <2441697223@qq.com>
2025-04-25 13:47:33 +0800

    feat: 增加jwt验证

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from 'src/user/user.module';
+import { jwtConstants } from './constants';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '1d' }, //  token过期时间
+    }),
+  ],
   providers: [AuthService],
   controllers: [AuthController],
 })

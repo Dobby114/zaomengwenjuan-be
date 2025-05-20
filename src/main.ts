3cd2a1649efb1a5d82d128f1835a1ac5b3822abb
Dobby114 <2441697223@qq.com>
2025-05-20 21:54:28 +0800

    fix: 修改允许跨域域名为前端项目域名

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { HttpExceptionFilter } from './http-exception/http-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://wenjuanxing-fe.vercel.app',
+    origin: 'https://zaomengwenjuan-fe.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // 允许的 HTTP 方法
     // allowedHeaders: 'Content-Type,Authorization', // 允许的请求头
     // credentials: true, // 是否允许发送 Cookie
